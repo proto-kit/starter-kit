e1edaccfe9b722272684e27f127bd252e17ad017
Matej Sima <matej.sima@gmail.com>
2023-11-06 10:52:47 +0100

    reinit web app

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Button>Button</Button>
+    </>
+  );
+}
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: false,
-    async headers() {
-        return [
-            {
-                "source": "/(.*)",
-                "headers": [
-                    { "key": "Cross-Origin-Opener-Policy", "value": "same-origin" },
-                    { "key": "Cross-Origin-Embedder-Policy", "value": "require-corp" }
-                ]
-            },
-        ]
-    },
-    webpack(config) {
-        config.experiments = { ...config.experiments, topLevelAwait: true };
-        return config;
-    },
-    compress: false,
-}
+const nextConfig = {}
 
 module.exports = nextConfig
