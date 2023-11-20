f1e07385fbeb26c44f413d260dc2ba3e91294662
asimaranov <a.simaranov@gmail.com>
2023-11-20 22:37:35 +0300

    fix: crush if window.mina is not defined

--- apps/web/lib/stores/wallet.tsx
+++ apps/web/lib/stores/wallet.tsx
@@ -25,7 +25,7 @@ export interface WalletState {
 export const useWalletStore = create<WalletState, [["zustand/immer", never]]>(
   immer((set) => ({
     async initializeWallet() {
-      if (!mina) {
+      if (typeof mina === 'undefined') {
         throw new Error("Auro wallet not installed");
       }
 
@@ -36,7 +36,7 @@ export const useWalletStore = create<WalletState, [["zustand/immer", never]]>(
       });
     },
     async connectWallet() {
-      if (!mina) {
+      if (typeof mina === 'undefined') {
         throw new Error("Auro wallet not installed");
       }
 
@@ -47,7 +47,7 @@ export const useWalletStore = create<WalletState, [["zustand/immer", never]]>(
       });
     },
     observeWalletChange() {
-      if (!mina) {
+      if (typeof mina === 'undefined') {
         throw new Error("Auro wallet not installed");
       }
 
