3da6395fc1cfdad677d579a96de4adb0085438f8
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2024-04-04 00:16:48 -0700

    enable configuration of sequencer URL with env

--- packages/chain/src/client.config.ts
+++ packages/chain/src/client.config.ts
@@ -5,6 +5,10 @@ const appChain = ClientAppChain.fromRuntime(runtime.modules);
 
 appChain.configurePartial({
   Runtime: runtime.config,
+  GraphqlClient: {
+    url:
+      process.env.NEXT_PUBLIC_PROTOKIT_URL || "http://localhost:8080/graphql",
+  },
 });
 
 export const client = appChain;
