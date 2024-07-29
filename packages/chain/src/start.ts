7197fa26278e17443b6344e9abce6ac204c4c5bb
Raphael Panic <raphael.panic@gmx.at>
2024-07-29 17:35:05 +0200

    A certain developer doesnt care about Windows support so now I had to pick up the pieces

--- packages/chain/src/start.ts
+++ packages/chain/src/start.ts
@@ -35,11 +35,39 @@ yargs(hideBin(process.argv))
     async (args) => {
       log.setLevel(args.logLevel);
 
-      const appChainFactory: AppChainFactory = (await import(args.appChain))
+      // For windows support, we need to parse out environment variables used in the path
+      let path = replaceEnvTemplates(args.appChain);
+
+      const appChainFactory: AppChainFactory = (await import(path))
         .default;
       const appChain = await appChainFactory(args);
 
       await appChain.start();
-    }
+    },
   )
   .parse();
+
+function replaceEnvTemplates(str: string) {
+  let temp = str;
+
+  const envRegex = /\$[A-Z1-9_]*/;
+
+  let m;
+  while ((m = envRegex.exec(temp)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === envRegex.lastIndex) {
+      envRegex.lastIndex++;
+    }
+
+    // The result can be accessed through the `m`-variable.
+    m.forEach((match, groupIndex) => {
+      const envVarName = match.slice(1);
+      const envVarValue = process.env[envVarName];
+      if(envVarValue === undefined) {
+        throw new Error(`Substituted environment variable ${envVarName} not found`)
+      }
+      temp = temp.replace(match, envVarValue);
+    });
+  }
+  return temp;
+}
