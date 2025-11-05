2d4a4b93d305cbbc6a3656af3ec153a108c4ab97
Kadircan Bozkurt <kadircanbozkurt1905@gmail.com>
2025-11-05 18:55:22 +0300

    add prettier config and change to error on missing await

--- packages/eslint-config-custom/library.js
+++ packages/eslint-config-custom/library.js
@@ -12,25 +12,26 @@ const project = resolve(process.cwd(), "tsconfig.json");
  */
 
 module.exports = {
-    extends: ["@vercel/style-guide/eslint/node", "@vercel/style-guide/eslint/typescript"].map(
-        require.resolve
-    ),
-    parserOptions: {
+  extends: [
+    "@vercel/style-guide/eslint/node",
+    "@vercel/style-guide/eslint/typescript",
+  ].map(require.resolve),
+  parserOptions: {
+    project,
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
         project,
+      },
     },
-    globals: {
-        React: true,
-        JSX: true,
-    },
-    settings: {
-        "import/resolver": {
-            typescript: {
-                project,
-            },
-        },
-    },
-    ignorePatterns: ["node_modules/", "dist/"],
-    rules: {
-        "@typescript-eslint/no-floating-promises": "warn",
-    },
+  },
+  ignorePatterns: ["node_modules/", "dist/"],
+  rules: {
+    "@typescript-eslint/no-floating-promises": "error",
+  },
 };
